13bd146e450d487a649497d0f99401ef709aa682
Lawynn Jana <jlawynn@gmail.com>
2025-11-23 11:47:30 -0500

    update

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -14,7 +14,7 @@ const GameScreen = ({
         initial: { opacity: 0, x: -20 },
         animate: { opacity: 1, x: 0 },
         exit: { opacity: 0, x: -100 },
-        transition: { duration: 0.3 }
+        transition: { duration: 0.15 }
     };
 
     return (
